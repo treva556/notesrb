0d2e08a90ab088ac0ac46d981a6ae13b2052e807
treva556 <marktreva92@gmail.com>
2024-11-17 03:33:57 +0300

    proto

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,42 @@
+
+class NotesController < ApplicationController
+    def index
+      notes = Note.all
+      render json: notes
+    end
+  
+    def show
+      note = Note.find(params[:id])
+      render json: note
+    end
+  
+    def create
+      note = Note.new(note_params)
+      if note.save
+        render json: note, status: :created
+      else
+        render json: note.errors, status: :unprocessable_entity
+      end
+    end
+  
+    def update
+      note = Note.find(params[:id])
+      if note.update(note_params)
+        render json: note
+      else
+        render json: note.errors, status: :unprocessable_entity
+      end
+    end
+  
+    def destroy
+      note = Note.find(params[:id])
+      note.destroy
+      head :no_content
+    end
+  
+    private
+  
+    def note_params
+      params.require(:note).permit(:title, :content)
+    end
+  end
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -14,3 +14,11 @@
 #       methods: [:get, :post, :put, :patch, :delete, :options, :head]
 #   end
 # end
+
+
+  Rails.application.config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins 'http://localhost:3001'  # React app running here
+      resource '*', headers: :any, methods: %i[get post put delete]
+    end
+  end
